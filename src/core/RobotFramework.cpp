9605cc5561fb098105512f7311f9adc5bef61afa
Milos Zivlak <zivlakmilos@gmail.com>
2020-02-26 19:09:07 +0100

    Build system

---
+++ src/core/RobotFramework.cpp
@@ -0,0 +1,30 @@
+/* 
+ * This file is part of the RPI Robot Framework distribution (https://github.com/Elektropioneer/RPI_Robot_Framework).
+ * Copyright (c) 2020 Miloš Zivlak (milos.zivlak@sensa-group.net).
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "core/RobotFramework.h"
+
+RobotFramework::RobotFramework(int argc, char *argv[])
+{
+}
+
+RobotFramework::~RobotFramework(void)
+{
+}
+
+void RobotFramework::run(void)
+{
+}
